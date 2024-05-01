11c0209d4f6b723f19fb4dfe96bb58b53ab58de9
daywave <al295042@edu.uaa.mx>
2024-05-01 17:52:08 -0600

    actualizacion

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,6 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-<<<<<<< HEAD
-    id("com.android.application") version "8.3.0" apply false
-=======
-    id("com.android.application") version "8.2.1" apply false
-    id("com.google.gms.google-services") version "4.4.1" apply false
->>>>>>> develop
+    id("com.android.application") version "8.3.0" apply false;
+    id("com.android.application") version "8.2.1" apply false;
+    id("com.google.gms.google-services") version "4.4.1" apply false;
 }
